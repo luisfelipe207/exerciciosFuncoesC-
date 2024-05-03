fbe7fd513ac65c4940721d58c1c0a4e2b40da80b
luisfelipe207 <162385788+luisfelipe207@users.noreply.github.com>
2024-05-03 01:55:06 +0000

    exercicios de funcoes

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// exercicios strigs e arrays // 
+// 1- //
+#include <iostream> 
+#include <string>
+using namespace std;
+
+int main() {
+string nome = "Luis Felipe ";
+void imprimirNome(const std::string& nome) {
+  std::cout << nome << std::endl;
+}
+int main() {
+  imprimirNome(nome);
+  return 0;
+}
+}
+// 2- //
+
+int main() {
+    
